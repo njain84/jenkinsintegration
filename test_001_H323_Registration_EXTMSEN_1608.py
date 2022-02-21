b12c5d0251eaff53962998c678e96e1f99aeb4a8
Jain <navneet.jain@harman.com>
2022-02-21 17:59:13 +0530

    add testprogram.py git push -u origin testprogram.py git remote add origin https://github.com/njain84/jenkinsintegration.git git commit -m add.

--- test_001_H323_Registration_EXTMSEN_1608.py
+++ test_001_H323_Registration_EXTMSEN_1608.py
@@ -1,7 +1,7 @@
 import unittest
 from selenium import webdriver
 import webbrowser
-from test import test_001_H323_Registration_EXTMSEN_1608
+from test import test_0001_H323_Registration_EXTMSEN_1608
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
@@ -27,7 +27,7 @@ class Smoketest(unittest.TestCase):
 
     @allure.step("Launch1 site")
     def launch_site(self):
-        smoke1 = test_001_H323_Registration_EXTMSEN_1608.H323
+        smoke1 = test_0001_H323_Registration_EXTMSEN_1608.H323
         smoke1.test_H323_Registration(self)
 
 
